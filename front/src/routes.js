f1febc566ce6fa407ba369a77e6c2c21f59b2db4
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-01-17 02:06:24 -0300

    Rota User

--- front/src/routes.js
+++ front/src/routes.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Posts from './pages/posts';
 import PostDetails from './pages/posts-details';
+import User from './pages/user';
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Posts} />
         <Route path="/post-details/:id" component={PostDetails} />
+        <Route path="/user/:id" component={User} />
     </Switch>
 );
 
